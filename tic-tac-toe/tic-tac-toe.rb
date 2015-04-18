990e1e289cbfe576161316e7f732e8f107de9a18
C Haden <chaden@spoonflower.com>
2015-04-17 22:24:55 -0400

    player makes random moves

--- tic-tac-toe/tic-tac-toe.rb
+++ tic-tac-toe/tic-tac-toe.rb
@@ -1,11 +1,18 @@
 class Board
 
+  BOARD_HEIGHT = 3
+  BOARD_WIDTH = 3
+
   def initialize
     # assuming only 3x3 boards exist
     # blank space = " ", marked by X = "X", marked by O = "O"
     @board = [[" ", " ", " "],
               [" ", " ", " "],
               [" ", " ", " "]]
+  end
+
+  def apply_move( coord, player )
+    @board[coord[0]][coord[1]] = player
   end
 
   def display_board
@@ -34,7 +41,31 @@ end
 
 class Player
 
+  def move( current_board )
+    r = Random.new(1234)
+    y = rand(0..Board::BOARD_HEIGHT-1)
+    puts "y = #{y}"
+    x = rand(0..Board::BOARD_WIDTH-1)
+    puts "x = #{x}"
+    return [x,y]
+  end
 end
 
 board = Board.new
+board.display_board
+
+puts " "
+
+player = Player.new
+board.apply_move( player.move( board ), "X" )
+board.display_board
+
+puts " "
+board.apply_move( player.move( board ), "0" )
+board.display_board
+puts " "
+board.apply_move( player.move( board ), "X" )
+board.display_board
+puts " "
+board.apply_move( player.move( board ), "0" )
 board.display_board
