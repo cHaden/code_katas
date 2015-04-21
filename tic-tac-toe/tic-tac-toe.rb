fdfce274547b3fe4fa1e1762b791b70fa87f826f
C Haden <chaden@spoonflower.com>
2015-04-21 19:47:31 -0400

    using matrix class, with extra method for editing coordinates

--- tic-tac-toe/tic-tac-toe.rb
+++ tic-tac-toe/tic-tac-toe.rb
@@ -1,3 +1,11 @@
+require 'matrix'
+
+class Matrix
+  def []=(x, y, value)
+    @rows[x][y] = value
+  end
+end
+
 class Board
 
   BOARD_HEIGHT = 3
@@ -6,39 +14,55 @@ class Board
   def initialize
     # assuming only 3x3 boards exist
     # blank space = " ", marked by X = "X", marked by O = "O"
-    @board = [[" ", " ", " "],
+    @board = Matrix[[" ", " ", " "],
               [" ", " ", " "],
               [" ", " ", " "]]
   end
 
   def blank?( coord )
-    @board[coord[0]][coord[1]] == " "
+    @board.to_a[coord[0]][coord[1]] == " "
   end
 
   def apply_move( coord, player )
-    @board[coord[0]][coord[1]] = player
+    @board[coord[0],coord[1]] = player
   end
 
   def display_board
     # assumes 2D board
-    @board.length.times do |y|
-      @board[0].length.times do |x|
-        print @board[x][y]
-        if x < @board[0].length - 1
+    @board.to_a.length.times do |y|
+      @board.row(0).to_a.length.times do |x|
+        print @board.to_a[x][y]
+        if x < @board.row(0).to_a.length - 1
           print '|'
         end
       end
 
       print "\n"
 
-      if y < @board.length - 1
-        ((@board[0].length*2)-1).times do |x|
+      if y < @board.to_a.length - 1
+        ((@board.row(0).to_a.length*2)-1).times do |x|
           print '-'
         end
         print "\n"
       end
 
     end
+  end
+
+  def player_has_winning_move( player_value )
+      @board.to_a.length.times do |x|
+        if @board.row(x).select {|value| value == player_value }.length == 2
+          if @board.row(x).index(" ")
+            return [x,@board.row(x).to_a.index(" ")]
+          end
+        end
+        if @board.column(x).select {|value| value == player_value }.length == 2
+          if @board.column(x).to_a.index(" ")
+            return [@board.column(x).to_a.index(" "),x]
+          end
+        end
+      end
+      return nil
   end
 
 end
@@ -66,11 +90,17 @@ class Player
 
   def move()
 
-    coord = random_move
-    @board.apply_move(coord, @player_type)
+    if coord = @board.player_has_winning_move( @player_type )
+      @board.apply_move(coord, @player_type)
+      puts "I think that player #{@player_type} has a winning move"
+    else
+      coord = random_move
+      @board.apply_move(coord, @player_type)
+      puts "Random move for #{@player_type}, coord #{coord}"
+    end
+
     @board.display_board
     puts " "
-    return coord
   end
 end
 
