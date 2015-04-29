643e47c36bdca54c495fc2feb32709bd57eef27d
C Haden <chaden@spoonflower.com>
2015-04-28 23:48:49 -0400

    vim practice. numbers to words, up to 3 digits

--- codingdojo_katas/numbers_to_words.rb
+++ codingdojo_katas/numbers_to_words.rb
@@ -42,8 +42,77 @@ def teen( number )
 	end
 end
 
+def tens_place( number )
+	first = number / 10
+	second = number % 10
+
+	string = ""
+
+	case first
+	when 1
+		return teen( number )
+	when 2
+		string = "twenty"
+	when 3
+		string = "thirty"
+	when 4
+		string = "forty"
+	when 5
+		string = "fifty"
+	when 6
+		string = "sixty"
+	when 7
+		string = "seventy"
+	when 8
+		string = "eighty"
+	when 9
+		string = "ninety"
+	end
+
+	string += "-" + tens(second) if second != 0
+
+	return string
+end
+
+def hundreds_place( number )
+	first = number / 100
+	second = number % 100
+
+	string = tens(first) + " hundred "
+
+	if second != 0
+		if second / 10 != 0
+			string += tens_place( second )
+		else
+			string += tens( second % 10 )
+		end
+	end
+
+	return string
+end 
+
+def number_to_word( number )
+		x = number.to_s.length
+		case x
+		when 1
+			tens( number )
+		when 2
+			tens_place( number )
+		when 3
+			hundreds_place( number )
+		end
+	end
 puts tens( 7 )
 
 puts tens( 0 )
 puts teen( 11)
 puts teen(19)
+
+puts number_to_word(4)
+puts number_to_word(20)
+puts number_to_word(49)
+puts number_to_word(14)
+puts number_to_word(11)
+puts number_to_word(100)
+puts number_to_word(204)
+puts number_to_word(739)
